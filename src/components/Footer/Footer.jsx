33d9559049bc1fa64e04d0668c28b4dcb609abb1
Saransh <saranshagarwal2004@gmail.com>
2024-11-19 16:48:46 +0530

    footer finalised

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -28,13 +28,13 @@ export default function Footer() {
             <h3 className="text-lg font-semibold text-white mb-4">Resources</h3>
             <ul className="space-y-4 text-gray-300">
               <li>
-                <Link to="/" className="hover:text-yellow-400 transition duration-300">Home</Link>
+                <Link to="/" className="hover:text-green-400 transition duration-300">Home</Link>
               </li>
               <li>
-                <Link to="/about" className="hover:text-yellow-400 transition duration-300">About</Link>
+                <Link to="/about" className="hover:text-green-400 transition duration-300">About</Link>
               </li>
               <li>
-                <Link to="/contact" className="hover:text-yellow-400 transition duration-300">Contact</Link>
+                <Link to="/contact" className="hover:text-green-400 transition duration-300">Contact</Link>
               </li>
             </ul>
           </div>
@@ -46,7 +46,7 @@ export default function Footer() {
               <li>
                 <a
                   href="https://www.linkedin.com/company/siamjuit"
-                  className="hover:text-yellow-400 transition duration-300"
+                  className="hover:text-green-400 transition duration-300"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
@@ -54,10 +54,25 @@ export default function Footer() {
                 </a>
               </li>
               <li>
-                <Link to="https://www.instagram.com/siamjuit/" className="hover:text-yellow-400 transition duration-300">Instagram</Link>
+                {/* <Link to="https://www.instagram.com/siamjuit/" className="hover:text-yellow-400 transition duration-300" >Instagram</Link> */}
+                <a
+                  href="https://www.instagram.com/siamjuit/"
+                  className="hover:text-green-400 transition duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Instagram
+                </a>
               </li>
               <li>
-                <Link to="https://x.com/siamjuit" className="hover:text-yellow-400 transition duration-300">Twitter</Link>
+              <a
+                  href="https://x.com/siamjuit"
+                  className="hover:text-green-400 transition duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Twitter
+                </a>
               </li>
             </ul>
           </div>
@@ -67,7 +82,14 @@ export default function Footer() {
             <h3 className="text-lg font-semibold text-white mb-4">Links</h3>
             <ul className="space-y-4 text-gray-300">
               <li>
-                <Link to="https://www.siam.org/" className="hover:text-yellow-400 transition duration-300">Siam Global</Link>
+              <a
+                  href="https://www.siam.org/"
+                  className="hover:text-green-400 transition duration-300"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Siam Global
+                </a>
               </li>
               {/* <li>
                 <Link to="#" className="hover:text-yellow-400 transition duration-300">Terms & Conditions</Link>
@@ -80,7 +102,7 @@ export default function Footer() {
         <div className="flex justify-center space-x-8 mb-8">
           <a
             href="https://www.linkedin.com/company/siamjuit"
-            className="text-gray-400 hover:text-blue-400 transition duration-300"
+            className="text-white-400 hover:text-blue-400 transition duration-300"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -94,28 +116,28 @@ export default function Footer() {
           </a>
           <a
             href="https://www.instagram.com/siamjuit/"
-            className="text-gray-400 hover:text-pink-400 transition duration-300"
+            className="text-white-400 hover:text-pink-400 transition duration-300"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <svg className="w-9 h-9" fill="none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 20">
+            
+            <svg className="w-9 h-9" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 20">
               <path
                 fillRule = "evenodd"
-                d="M12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm0-2a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm6.5-.25a1.25 1.25 0 0 1-2.5 0 1.25 1.25 0 0 1 2.5 0zM12 4c-2.474 0-2.878.007-4.029.058-.784.037-1.31.142-1.798.332-.434.168-.747.369-1.08.703a2.89 2.89 0 0 0-.704 1.08c-.19.49-.295 1.015-.331 1.798C4.006 9.075 4 9.461 4 12c0 2.474.007 2.878.058 4.029.037.783.142 1.31.331 1.797.17.435.37.748.702 1.08.337.336.65.537 1.08.703.494.191 1.02.297 1.8.333C9.075 19.994 9.461 20 12 20c2.474 0 2.878-.007 4.029-.058.782-.037 1.309-.142 1.797-.331.433-.169.748-.37 1.08-.702.337-.337.538-.65.704-1.08.19-.493.296-1.02.332-1.8.052-1.104.058-1.49.058-4.029 0-2.474-.007-2.878-.058-4.029-.037-.782-.142-1.31-.332-1.798a2.911 2.911 0 0 0-.703-1.08 2.884 2.884 0 0 0-1.08-.704c-.49-.19-1.016-.295-1.798-.331C14.925 4.006 14.539 4 12 4zm0-2c2.717 0 3.056.01 4.122.06 1.065.05 1.79.217 2.428.465.66.254 1.216.598 1.772 1.153a4.908 4.908 0 0 1 1.153 1.772c.247.637.415 1.363.465 2.428.047 1.066.06 1.405.06 4.122 0 2.717-.01 3.056-.06 4.122-.05 1.065-.218 1.79-.465 2.428a4.883 4.883 0 0 1-1.153 1.772 4.915 4.915 0 0 1-1.772 1.153c-.637.247-1.363.415-2.428.465-1.066.047-1.405.06-4.122.06-2.717 0-3.056-.01-4.122-.06-1.065-.05-1.79-.218-2.428-.465a4.89 4.89 0 0 1-1.772-1.153 4.904 4.904 0 0 1-1.153-1.772c-.248-.637-.415-1.363-.465-2.428C2.013 15.056 2 14.717 2 12c0-2.717.01-3.056.06-4.122.05-1.066.217-1.79.465-2.428a4.88 4.88 0 0 1 1.153-1.772A4.897 4.897 0 0 1 5.45 2.525c.638-.248 1.362-.415 2.428-.465C8.944 2.013 9.283 2 12 2z"  fill="white"
+                d="M12 9a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm0-2a5 5 0 1 1 0 10 5 5 0 0 1 0-10zm6.5-.25a1.25 1.25 0 0 1-2.5 0 1.25 1.25 0 0 1 2.5 0zM12 4c-2.474 0-2.878.007-4.029.058-.784.037-1.31.142-1.798.332-.434.168-.747.369-1.08.703a2.89 2.89 0 0 0-.704 1.08c-.19.49-.295 1.015-.331 1.798C4.006 9.075 4 9.461 4 12c0 2.474.007 2.878.058 4.029.037.783.142 1.31.331 1.797.17.435.37.748.702 1.08.337.336.65.537 1.08.703.494.191 1.02.297 1.8.333C9.075 19.994 9.461 20 12 20c2.474 0 2.878-.007 4.029-.058.782-.037 1.309-.142 1.797-.331.433-.169.748-.37 1.08-.702.337-.337.538-.65.704-1.08.19-.493.296-1.02.332-1.8.052-1.104.058-1.49.058-4.029 0-2.474-.007-2.878-.058-4.029-.037-.782-.142-1.31-.332-1.798a2.911 2.911 0 0 0-.703-1.08 2.884 2.884 0 0 0-1.08-.704c-.49-.19-1.016-.295-1.798-.331C14.925 4.006 14.539 4 12 4zm0-2c2.717 0 3.056.01 4.122.06 1.065.05 1.79.217 2.428.465.66.254 1.216.598 1.772 1.153a4.908 4.908 0 0 1 1.153 1.772c.247.637.415 1.363.465 2.428.047 1.066.06 1.405.06 4.122 0 2.717-.01 3.056-.06 4.122-.05 1.065-.218 1.79-.465 2.428a4.883 4.883 0 0 1-1.153 1.772 4.915 4.915 0 0 1-1.772 1.153c-.637.247-1.363.415-2.428.465-1.066.047-1.405.06-4.122.06-2.717 0-3.056-.01-4.122-.06-1.065-.05-1.79-.218-2.428-.465a4.89 4.89 0 0 1-1.772-1.153 4.904 4.904 0 0 1-1.153-1.772c-.248-.637-.415-1.363-.465-2.428C2.013 15.056 2 14.717 2 12c0-2.717.01-3.056.06-4.122.05-1.066.217-1.79.465-2.428a4.88 4.88 0 0 1 1.153-1.772A4.897 4.897 0 0 1 5.45 2.525c.638-.248 1.362-.415 2.428-.465C8.944 2.013 9.283 2 12 2z" 
               />
             </svg>
           </a>
           <a
             href="https://twitter.com"
-            className="text-gray-400 hover:text-black-400 transition duration-300"
+            className="text-white-400 hover:text-gray-700 transition duration-300"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <svg className="w-8 h-8" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 21">
+            <svg className="w-8 h-9" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
               <path
                 fillRule="evenodd"
                 d="M18.3263 2.03711H21.6998L14.3297 10.4606L23 21.9231H16.2112L10.894 14.9711L4.80995 21.9231H1.43443L9.31743 12.9132L1 2.03711H7.96111L12.7674 8.39144L18.3263 2.03711ZM17.1423 19.9039H19.0116L6.94539 3.95024H4.93946L17.1423 19.9039Z"
-                clipRule="evenodd"
               />
             </svg>
           </a>
